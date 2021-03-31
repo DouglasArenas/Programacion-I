14ca37bcb6964081a9fa8819d003e30fbc86ab84
Douglas Arenas <douglasarenas71@gmail.com>
2021-03-30 21:07:17 -0300

    dir resources

---
+++ backend/main/resources/__init__.py
@@ -0,0 +1,9 @@
+from .Bolsones import recursos as Bolsonesrecurso
+from .Bolson import recursos as Bolsonrecurso
+from .Bolsones Venta import recursos as BolsonesVentarecurso
+from .Bolson Venta import recursos as BolsonVentarecurso
+from .Bolsones Pendientes import recursos as BolsonesPendiestesrecurso
+from .Bolson Pendiente import recursos as BolsonPendienterecurso
+from .Bolsones Previos import recursos as BolsonesPreviosrecurso
+from .Productos import recursos as Productosrecurso
+from .Producto import recursos as
