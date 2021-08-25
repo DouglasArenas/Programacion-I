52cc4a26ca1b3a85e92afb53735af51a37db5b58
Douglas Arenas <douglasarenas71@gmail.com>
2021-08-25 10:19:13 -0300

    html y rutas

--- frontend/main/__init__.py
+++ frontend/main/__init__.py
@@ -5,4 +5,8 @@ from dotenv import load_dotenv
 def create_app():
     app = Flask(__name__)
     load_dotenv()
+
+    from main.routes import main
+    app.register_blueprint(routes.main.main)
+    
     return app
