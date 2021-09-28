4aa3065dda6989828949112165ec978a28083dea
Douglas Arenas <douglasarenas71@gmail.com>
2021-09-27 21:49:15 -0300

    templates

--- frontend/main/routes/main.py
+++ frontend/main/routes/main.py
@@ -1,7 +1,37 @@
-from flask import Blueprint, url_for, render_template
+from flask import Blueprint, url_for, render_template, redirect, current_app
+from main.forms import register_form, login_form
+import requests, json
 
 main = Blueprint('main', __name__, url_prefix='/')
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title='Venta de bolsones')
+
+@main.route('/register', methods=['POST', 'GET'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = {
+            'nombre': form.nombre.data,
+            'apellido': form.apellido.data,
+            'telefono': form.telefono.data,
+            'mail': form.email.data,
+            'password': form.password.data
+        }
+        r = requests.post(
+            f'{current_app.config["API_URL"]}/auth/register', json=user
+        )
+        if r.status_code == 201:
+            return redirect(url_for('main.login'))
+
+    return render_template('register.html', title='Register', bg_color="bg-secondary", form = form)
+
+@main.route('/login', methods=['POST', 'GET'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', bg_color="bg-secondary", form = form)
+
+@main.route('/logout')
+def logout():
+    return redirect(url_for('main.index'))
