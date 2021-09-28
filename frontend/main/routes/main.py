a452a7523ee04972dca01baf91dfaec0d038e453
DouglasArenas <douglasarenas71@gmail.com>
2021-09-28 20:43:52 -0300

    arreglo archivos

--- frontend/main/routes/main.py
+++ frontend/main/routes/main.py
@@ -6,31 +6,31 @@ main = Blueprint('main', __name__, url_prefix='/')
 
 @main.route('/')
 def index():
-    return render_template('index.html', title='Venta de bolsones')
+    return render_template('principal.html', title='Venta de bolsones')
 
 @main.route('/register', methods=['POST', 'GET'])
 def register():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        user = {
-            'nombre': form.nombre.data,
-            'apellido': form.apellido.data,
-            'telefono': form.telefono.data,
-            'mail': form.email.data,
-            'password': form.password.data
-        }
-        r = requests.post(
-            f'{current_app.config["API_URL"]}/auth/register', json=user
-        )
-        if r.status_code == 201:
-            return redirect(url_for('main.login'))
+    # form = RegisterForm()
+    # if form.validate_on_submit():
+    #     user = {
+    #         'nombre': form.nombre.data,
+    #         'apellido': form.apellido.data,
+    #         'telefono': form.telefono.data,
+    #         'mail': form.email.data,
+    #         'password': form.password.data
+    #     }
+    #     r = requests.post(
+    #         f'{current_app.config["API_URL"]}/auth/register', json=user
+    #     )
+    #     if r.status_code == 201:
+    #         return redirect(url_for('main.login'))
 
-    return render_template('register.html', title='Register', bg_color="bg-secondary", form = form)
+    return render_template('registro.html', title='Register')
 
 @main.route('/login', methods=['POST', 'GET'])
 def login():
     form = LoginForm()
-    return render_template('login.html', title='Login', bg_color="bg-secondary", form = form)
+    return render_template('inicio_sesion.html', title='Login', bg_color="bg-secondary", form = form)
 
 @main.route('/logout')
 def logout():
