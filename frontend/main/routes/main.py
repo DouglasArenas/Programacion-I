68eecf4125a0864c5f8b3fdd3ca82a38d7b4e2df
Douglas <douglasarenas71@gmail.com>
2021-10-05 20:55:16 -0300

    arg

--- frontend/main/routes/main.py
+++ frontend/main/routes/main.py
@@ -10,27 +10,13 @@ def index():
 
 @main.route('/register', methods=['POST', 'GET'])
 def register():
-    # form = RegisterForm()
-    # if form.validate_on_submit():
-    #     user = {
-    #         'nombre': form.nombre.data,
-    #         'apellido': form.apellido.data,
-    #         'telefono': form.telefono.data,
-    #         'mail': form.email.data,
-    #         'password': form.password.data
-    #     }
-    #     r = requests.post(
-    #         f'{current_app.config["API_URL"]}/auth/register', json=user
-    #     )
-    #     if r.status_code == 201:
-    #         return redirect(url_for('main.login'))
 
     return render_template('registro.html', title='Register')
 
 @main.route('/login', methods=['POST', 'GET'])
 def login():
-    form = LoginForm()
-    return render_template('inicio_sesion.html', title='Login', bg_color="bg-secondary", form = form)
+    #form = LoginForm()
+    return render_template('inicio_sesion.html', title='Login') #form = form)
 
 @main.route('/logout')
 def logout():
